53b0f5efa87c89d2b7e27136affe17c164a43890
lucas.cardoso <lucascpereira39@gmail.com>
2026-05-09 01:58:39 -0300

    v0.1

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
